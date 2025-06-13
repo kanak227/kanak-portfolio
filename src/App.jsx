0edd759d288ef8bc229d4cd99c9dcdc6e4390dd7
kanak227 <kanakverma325@gmail.com>
2025-06-14 00:44:41 +0530

    initial copy

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LoadingScreen from './components/LoadingScreen'
+import './index.css'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  const [isLoaded , setIsLoaded] = useState(false);
+  const [menuOpen , setmenuOpen] = useState(false);
+
+
+  return (
+    <>
+   {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+
+    <div className='{min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 } '>
+      
+    <Navbar menuOpen ={menuOpen} setmenuOpen = {setmenuOpen}/>
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
